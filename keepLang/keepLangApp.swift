56a913747c1b6fd61e93a3f2e151f471e409b091
Shay Moreno <moa129@gmail.com>
2023-07-12 22:31:38 +0300

    Initial Commit

---
+++ keepLang/keepLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepLangApp.swift
+//  keepLang
+//
+//  Created by shay moreno on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct keepLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
